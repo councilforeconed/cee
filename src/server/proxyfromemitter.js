9de701e42e1cb864b45662ca7830eb9c246e5557
Michael "Z" Goddard <mzgoddard@gmail.com>
2014-04-11 17:28:46 -0400

    fixup! Add ProxyMe. Emits duplicate events under name '*'

--- src/server/proxyfromemitter.js
+++ src/server/proxyfromemitter.js
@@ -3,28 +3,28 @@
 // Node core modules
 var EventEmitter = require('events').EventEmitter;
 
-// @export ProxyMe
-module.exports = ProxyMe;
+// @export ProxyFromEmitter
+module.exports = ProxyFromEmitter;
 
 var slice = [].slice.call.bind([].slice);
 
 // A small extension to EventEmitter behaviour that emits events normally than
 // emits a special '*' event with the name of of the event as the first
 // argument followed by the event's arguments.
-function ProxyMe() {
+function ProxyFromEmitter() {
   EventEmitter.call(this);
 }
 
 // @inherits {EventEmitter}
-ProxyMe.prototype = Object.create(EventEmitter.prototype);
-ProxyMe.prototype.constructor = ProxyMe;
+ProxyFromEmitter.prototype = Object.create(EventEmitter.prototype);
+ProxyFromEmitter.prototype.constructor = ProxyFromEmitter;
 
 // Store the normal emit for easy use.
-ProxyMe.prototype._emit = EventEmitter.prototype.emit;
+ProxyFromEmitter.prototype._emit = EventEmitter.prototype.emit;
 
 // Emit an event and then a special '*' event including the name of the
 // original event as the first argument.
-ProxyMe.prototype.emit = function() {
+ProxyFromEmitter.prototype.emit = function() {
   this._emit.apply(this, arguments);
 
   var starArgs = slice(arguments);
